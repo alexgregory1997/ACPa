651cbe3e37cd7c6f1fe06ccb10201c119c1a43f9
Alex Gregory <alexgregory1997@gmail.com>
2018-10-05 13:51:40 +0100

    updated .f90 with minimum functionality

--- approxing_pi.f90
+++ approxing_pi.f90
@@ -2,14 +2,23 @@ program approxing_pi
   implicit none
   integer, parameter :: dp=selected_real_kind(15,300)
   real(kind=dp)      :: approx_pi, approx_func, rms_error
-  integer            :: i, n, ios, n_start, n_final, n_spacing
+  integer            :: i, n, ios, n_start, n_final, n_spacing, count, ierr, dim
+  real(kind=dp), dimension(:), allocatable :: store_pi_approx
 
   ! initialise variables
   approx_pi = 0.0_dp
+  count = 1
 
   read*, n_start
   read*, n_final
   read*, n_spacing
+
+  ! dimensions of array
+  dim = ((n_final-n_start)/n_spacing)+1
+
+  ! allocate array
+  allocate (store_pi_approx(3*dim),stat=ierr)
+  if (ierr.ne.0) stop 'error allocating store_pi_approx array'
 
   ! open files
   open(unit=50, file='pi.dat', iostat=ios)
@@ -26,14 +35,33 @@ program approxing_pi
     ! write what n it ran for, the approximation of pi and the root-mean-square error
     write(50,*) n, approx_pi, rms_error(approx_pi)
 
+    ! store error, N and Pi approximation
+    store_pi_approx(count)   = rms_error(approx_pi)
+    store_pi_approx(count+1) = N
+    store_pi_approx(count+2) = approx_pi
+
+    ! count the number of iterations, adding 3 to avoid overwriting data
+    count = count + 3
+
     ! reinitialise for new n
     approx_pi = 0.0_dp
 
   end do
 
+  ! print minimum value of pi found between two N values and associated N and RMSE
+  ! note - I can use this method since the RMSE will always be the smallest value within the array
+  ! and associated N and Pi is stored with it
+  print*, 'Best Pi Approx:  ', store_pi_approx(minloc(store_pi_approx)+2)
+  print*, 'Associated N:    ', store_pi_approx(int(minloc(store_pi_approx)+1))
+  print*, 'Associated RMSE: ', minval(store_pi_approx)
+
   ! close files
   close(unit=50,iostat=ios)
   if (ios.ne.0) stop 'error closing pi.dat file'
+
+  ! deallocate arrays
+  deallocate(store_pi_approx,stat=ierr)
+  if (ierr.ne.0) stop 'error deallocating store_pi_approx array'
 
 end program approxing_pi
 
